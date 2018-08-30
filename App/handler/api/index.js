bd1e63839a06b4b44a6a3448f4ca426e5c9cb37e
Shodiqul Muzaki <user.creativefull@gmail.com>
2018-08-30 11:41:14 +0700

    add detail of tmp Deals

--- App/handler/api/index.js
+++ App/handler/api/index.js
@@ -22,11 +22,12 @@ module.exports = exports = function (app) {
 
     app.post('/api/tmpDeals/push', (req,res,next) => {
         const b = req.body
-        let _id = b.id
+        let _id = b._id
         b.updated_at = new Date()
 
-        req.db.SaveData('tmpDeals', {$set : b}, (err, results) => {
+        req.db.update('tmpDeals', {_id : _id}, {$set : b}, (err, results) => {
             if (err) {
+                console.error(err)
                 return res.status(500).json({
                     status : 500,
                     message : 'Server Error, Please contact us'
@@ -36,6 +37,42 @@ module.exports = exports = function (app) {
             return res.status(200).json({
                 status : 200,
                 data : b
+            })
+        })
+    })
+
+    app.get('/api/tmpDeals/list/:uid', (req,res,next) => {
+        const uid = req.params.uid
+        req.db.find('tmpDeals', {uid : uid}, {}, (err, results) => {
+            if (err) {
+                console.error(err)
+                return res.status(500).json({
+                    status : 500,
+                    message : 'Server Error, Please contact us'
+                })
+            }
+
+            return res.status(200).json({
+                status : 200,
+                data : results
+            })
+        })
+    })
+
+    app.get('/api/tmpDeals/:id', (req,res,next) => {
+        const _id = req.params.id
+        req.db.findOne('tmpDeals', {_id : _id}, (err, results) => {
+            if (err) {
+                console.error(err)
+                return res.status(500).json({
+                    status : 500,
+                    message : 'Server Error, Please contact us'
+                })
+            }
+            
+            return res.status(200).json({
+                status : 200,
+                data : results
             })
         })
     })
